353466511ef4ad71190b11b50ddd188aa8a33c79
Yiyi Sun <yiyisun@gmail.com>
2019-01-15 15:33:20 -0500

    add apprun app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,39 @@ import app from 'apprun';
 const model = 'Hello world - AppRun !';
 
 const view = (state) => <div>
-  <h1>{state}</h1>
+  <div className="row">
+    <div className="col-md-12 col-sm-12 col-xs-12">
+      <div className="x_panel">
+        <div className="x_title">
+          <h2>Plain Page</h2>
+          <ul className="nav navbar-right panel_toolbox">
+            <li><a className="collapse-link"><i className="fa fa-chevron-up"></i></a>
+            </li>
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
+                className="fa fa-wrench"></i></a>
+              <ul className="dropdown-menu" role="menu">
+                <li><a href="#">Settings 1</a>
+                </li>
+                <li><a href="#">Settings 2</a>
+                </li>
+              </ul>
+            </li>
+            <li><a className="close-link"><i className="fa fa-close"></i></a>
+            </li>
+          </ul>
+          <div className="clearfix"></div>
+        </div>
+        <div className="x_content">
+          {state}
+        </div>
+      </div>
+    </div>
+  </div>
 </div>;
 
 const update = {
 
 }
 
-app.start('my-app', model, view, update);
+app.start('apprun-app', model, view, update);
